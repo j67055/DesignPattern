a88a88bd41c9a4c4a387c5a518cd40a100cc6b64
SeongHyeok Jin <j67055@naver.com>
2022-06-20 18:41:07 +0900

    feat: Add Singleton

---
+++ Creational/Singleton/Singleton/GameMenu/GameMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class GameMenu
+    {
+        public static GameMenu Instance => instance == null ? instance = new GameMenu() : instance;
+
+        private static GameMenu instance;
+
+        private GameMenu() { }
+
+        public void pause()
+            => Console.WriteLine("Pause Game");
+        public void resume()
+            => Console.WriteLine("Resume Game");
+        public void exit()
+            => Console.WriteLine("Exit Game");
+    }
+}
